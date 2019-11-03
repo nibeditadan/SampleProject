156ded839920aed78055681298c9aa90f25ec888
Nibedita Dan <nibeditadan@gmail.com>
2019-11-03 22:37:29 +0530

    Configuring Extenttestreport

--- src/test/java/sampleProject/SampleProjectExecution.java
+++ src/test/java/sampleProject/SampleProjectExecution.java
@@ -1,6 +1,7 @@
 package sampleProject;
 
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -19,7 +20,7 @@ public class SampleProjectExecution {
 			if (browser.equalsIgnoreCase("Chrome")) {
 				System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//drivers//chromedriver.exe");
 				WebDriver driver=new ChromeDriver();
-				driver.get("https://mycase2-stage.cloudapps.cisco.com/case");
+				driver.get("https://google.co.in");
 				driver.findElement(By.id("userInput")).sendKeys("charhall");
 				driver.findElement(By.id("passwordInput")).sendKeys("welcome");
 				driver.findElement(By.id("login-button")).click();
@@ -27,23 +28,11 @@ public class SampleProjectExecution {
 				System.out.println("this is to test merge");
 				
 			}
-			else if(browser.equalsIgnoreCase("IE")){
-				System.setProperty("webdriver.ie.driver",System.getProperty("user.dir")+"//drivers//IEDriverServer.exe");
-				DesiredCapabilities capability = DesiredCapabilities.internetExplorer();
-				capability.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-				capability.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);				
-				WebDriver driver = new InternetExplorerDriver(capability);					
-				driver.get("https://mycase2-stage.cloudapps.cisco.com/case");
-				driver.findElement(By.id("userInput")).sendKeys("charhall");
-				driver.findElement(By.id("passwordInput")).sendKeys("welcome");
-				driver.findElement(By.id("login-button")).click();
-				
-				
-			}
+			
 			else if(browser.equalsIgnoreCase("Firefox")){
 				System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"//drivers//geckodriver.exe");
 				WebDriver driver=new FirefoxDriver();
-				driver.get("https://mycase2-stage.cloudapps.cisco.com/case");
+				driver.get("https://google.co.in");
 				driver.findElement(By.id("userInput")).sendKeys("charhall");
 				driver.findElement(By.id("passwordInput")).sendKeys("welcome");
 				driver.findElement(By.id("login-button")).click();
